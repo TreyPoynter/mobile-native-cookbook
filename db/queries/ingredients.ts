9f4fbe3578b629de7ec529f0bbf0427c17be702f
TreyPoynter <treypoynter@gmail.com>
2024-12-04 15:02:17 -0600

    basic query test
    doesn't work. need to initialize it into a writable directory

---
+++ db/queries/ingredients.ts
@@ -0,0 +1,14 @@
+import * as SQLite from 'expo-sqlite';
+
+export async function getIngredients() {
+  try {
+    const db = await SQLite.openDatabaseAsync('ingredients.db');
+
+    const allRows = await db.getAllAsync('SELECT * FROM ingredients ORDER BY name');
+    console.log(allRows);
+    return allRows;
+  } catch (error) {
+    console.log(error)
+  }
+
+}
