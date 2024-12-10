2a1da02bf3947399151ae857d74b613f80330aa6
TreyPoynter <treypoynter@gmail.com>
2024-12-10 08:37:30 -0600

    Create ProgressTracker.tsx

---
+++ components/ui/ProgressTracker.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export type ProgressTrackerPage = {
+  component: React.JSX.Element
+  pageId: string  // helped with clarifying which component to load on click.
+}
+
+export default function ProgressTracker() {
+
+}
